397c24591f15b331926083d425ba043792d4d09a
yash06chourasia <yashchourasia06022004@gmail.com>
2025-06-26 09:36:43 +0530

    update

--- deci_to_hex.cpp
+++ deci_to_hex.cpp
@@ -1,57 +1,28 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//     unsigned int decimal;
-//     int index = 0;
-//     char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-//     char ans[100];
-//     cout << "Enter a decimal number: ";
-//     cin >> decimal;
-//     if (decimal == 0) {
-//         cout << "Hexadecimal: 0";
-//         return 0 ;
-//     }
-//     while (decimal > 0) {
-//         int remainder = decimal % 16; 
-//         ans[index] = hexDigits[remainder];
-//         index++;
-//         decimal /= 16;  
-//     }
-//     cout << "Hexadecimal: ";
-//     for (int i = index - 1; i >= 0; i--) { 
-//         cout << ans[i];
-//     }
-//     return 0;
-// }
 #include <iostream>
 using namespace std;
-
 int main() {
     unsigned int decimal;
     int index = 0;
-    char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-                        'A', 'B', 'C', 'D', 'E', 'F'};
-    char ans[100]; // Array to store hex digits
-
+    char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    char ans[100];
     cout << "Enter a decimal number: ";
     cin >> decimal;
-
     if (decimal == 0) {
-        cout << "Hexadecimal: 0" << endl;
-        return 0;
+        cout << "Hexadecimal: 0";
+        return 0 ;
     }
-
     while (decimal > 0) {
-        int remainder = decimal % 16;  
-        ans[index++] = hexDigits[remainder]; // Corrected storage with increment
-        decimal /= 16;
+        int remainder = decimal % 16; 
+        ans[index] = hexDigits[remainder];
+        index++;
+        decimal /= 16;  
     }
-
     cout << "Hexadecimal: ";
     for (int i = index - 1; i >= 0; i--) { 
-        cout << ans[i]; // Print stored hex digits in reverse order
+        cout << ans[i];
     }
-    cout << endl;
-
     return 0;
 }
+
+
+
